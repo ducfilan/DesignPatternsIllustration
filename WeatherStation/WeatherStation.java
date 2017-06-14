b4c119cbfdaf5e4d6dae4dcf4cbd1772487a1bff
Duc Filan <DucFilan@gmail.com>
2017-06-14 15:28:12 +0900

    Implement Heat Index display

--- WeatherStation/WeatherStation.java
+++ WeatherStation/WeatherStation.java
@@ -2,7 +2,8 @@ public class WeatherStation {
     public static void main(String[] args){
         WeatherData weatherData = new WeatherData();
 
-        CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+        CurrentConditionsDisplay currentDisplay   = new CurrentConditionsDisplay(weatherData);
+        HeatIndexDisplay         heatIndexDisplay = new HeatIndexDisplay(weatherData);
 
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(82, 70, 29.2f);
